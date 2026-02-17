800ba419b35b4c7aa0edd50544b47c50b933e162
walysson <walyssongabriel14@hotmail.com>
2026-02-17 16:37:39 -0300

    script do sql, modelo usuário

---
+++ src/modelos/usuario.go
@@ -0,0 +1,13 @@
+package modelos
+
+import "time"
+
+//aqui estou criando o modelo de usuário que será inserido no banco
+type Usuario struct {
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
